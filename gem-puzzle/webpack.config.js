1f4ff8d8002e5b5d3740d5d410178ae5f4752a38
Kris Casper <sw.darkwind@gmail.com>
2020-11-05 00:58:08 +0300

    init: configure webpack with sass and babel loaders

---
+++ gem-puzzle/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'eval',
+  plugins: [new MiniCssExtractPlugin({
+    filename: 'style.css',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader'],
+      },
+    ]
+  }  
+};
